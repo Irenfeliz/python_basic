21bb62984fa1fcec07cd3349627bd37853b2c2b1
Irina <66956266+Irenfeliz@users.noreply.github.com>
2023-02-26 22:26:57 +0000

    first test

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-a1 = int(input())
-b1 = int(input())
-a2 = int(input())
-b2 = int(input())
+# put your python code here
+col1 = int(input())
+row1 = int(input())
+col2 = int(input())
+row2 = int(input())
 
-# a1 < b1, a2 < b2
+x1 = col1 % 2
+y1 = row1 % 2
 
-if a1 < a2 < b1 < b2:
-    print(a2, b1)
-elif a1 < a2 < b2 < b1:
-    print(a2, b2)
-elif a2 < a1 < b2 < b1:
-    print(a1, b2)
-elif a2 < a1 < b1 < b2:
-    print(a1, b1)   
-elif a1 == a2:
-    if b1 < b2:
-        print(a1, b1)
-    else:
-        print(a1,b2)
-elif b1 == b2:
-    if a1 < a2:
-        print(a2, b1)
-    else:
-        print(a1, b1)
-elif a2 == b1:
-    print(a2)
-elif a1 == b2:
-    print(a1)
+x2 = col2 % 2
+y2 = row2 % 2
+
+color1 = 0
+color2 = 0
+
+if x1 != 0 and y1 != 0:
+    color1 = 1
+elif x1 == 0 and y1 == 0:
+    color1 = 1
 else:
-    print('пустое множество')
+    color1 = 0
+print(x1, y1, color1)
+
+if x2 != 0 and y2 != 0:
+    color2 = 1
+elif x2 == 0 and y2 == 0:
+    color2 = 1
+else:
+    color2 = 0   
+
+print(x2, y2, color1)
+
+if color1 == color2:
+    print('YES')
+else:
+    print('NO')
