6a2fec0d0a9e804daae10bd8190ccb4da54b03a3
Irina <66956266+Irenfeliz@users.noreply.github.com>
2023-02-27 15:34:03 +0000

    new pazzle

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
-# put your python code here
-col1 = int(input())
-row1 = int(input())
-col2 = int(input())
-row2 = int(input())
+x = int(input())
 
-x1 = col1 % 2
-y1 = row1 % 2
+a = x // 100 
+b = x // 10 % 10
+c = x % 10
 
-x2 = col2 % 2
-y2 = row2 % 2
-
-color1 = 0
-color2 = 0
-
-if x1 != 0 and y1 != 0:
-    color1 = 1
-elif x1 == 0 and y1 == 0:
-    color1 = 1
-else:
-    color1 = 0
-print(x1, y1, color1)
-
-if x2 != 0 and y2 != 0:
-    color2 = 1
-elif x2 == 0 and y2 == 0:
-    color2 = 1
-else:
-    color2 = 0   
-
-print(x2, y2, color1)
-
-if color1 == color2:
-    print('YES')
-else:
-    print('NO')
+print(a, b, c)
