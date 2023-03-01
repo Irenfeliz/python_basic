d8aa37679f60af9c3f547149098782c8b3d34e83
Irina <66956266+Irenfeliz@users.noreply.github.com>
2023-03-01 14:56:35 +0000

    ugly pazzle

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-count = 0
+n = int(input())
+f0 = 0
+f1 = 1
 
-for i in range(10):
-    n = int(input())
-    if n % 2 == 0:
-        count += 1
-
-if count == 10:
-    print('YES')
-else:
-    print('NO')
+for i in range(n):
+  f0, f1 = f1, f0 + f1
+  print(f0, end=' ')
